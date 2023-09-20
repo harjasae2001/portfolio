f7c86d9e577e4b0e402e81a305e0d0cc33c4dec9
Harjas Singh <83627055+harjasae2001@users.noreply.github.com>
2023-09-21 02:37:40 +0530

    Added new files

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+import RealEst from "../assets/projects/realestate.jpg";
+import cryptoApp from "../assets/projects/cryptoApp.webp";
+import eCommerce from "../assets/projects/e-commerce.jpg";
+import weatherApp from "../assets/projects/weatherApp.jpg";
+import mapty from "../assets/projects/mapty.jpg";
+import netflixClone from "../assets/projects/netflix-clone.jpg";
+
+export const data = [
+  {
+    id: 1,
+    name: "CryptoApp",
+    image: cryptoApp,
+    github: "https://github.com/harjasae2001/CryptoApp",
+    live: "https://stockcrypto.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Realtor",
+    image: RealEst,
+    github: "https://github.com/harjasae2001/Real_Estate_Finder",
+    live: "https://real-estate-finder-harjasae2001.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "E - Commerce",
+    image: eCommerce,
+    github: "https://github.com/harjasae2001/E-Commerce",
+    live: "https://e-commerce-8e1p5hzf5-harjasae2001.vercel.app/",
+  },
+  {
+    id: 4,
+    name: "Weather App",
+    image: weatherApp,
+    github: "https://github.com/harjasae2001/Weather_App",
+    live: "https://weatherappupdate.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Mapty",
+    image: mapty,
+    github: "https://github.com/harjasae2001/Mapty",
+    live: "https://mapty-health-tracker.netlify.app/",
+  },
+  {
+    id: 4,
+    name: "Netflix Clone",
+    image: netflixClone,
+    github: "https://github.com/harjasae2001/Netflix-Clone",
+    live: "https://netflix-clone1609.netlify.app/",
+  },
+];
